99a699d7a88b5694332ae8d83a66da44d0c2b991
Jakub Kotkowiak <cckotkowiak@gmail.com>
2021-08-31 14:11:18 +0200

    Allow for changing of ansible-lint version

--- .github/actions/installation/install.sh
+++ .github/actions/installation/install.sh
@@ -1,9 +1,10 @@
-VERSION=${1:-2.9.6}
+ANSIBLE_VERSION=${1:-2.11.3}
+ANSIBLE_LINT_VERSION=${2:-5.1.3}
 
 sudo apt-get install update -y
 sudo apt-get install pip3
-sudo pip3 install ansible==${VERSION}
-sudo pip3 install ansible-lint
+sudo pip3 install ansible==${ANSIBLE_VERSION}
+sudo pip3 install ansible-lint==${ANSIBLE_LINT_VERSION}
 
 ansible --version
 ansible-lint --version
