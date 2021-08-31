7a742e6fedf01974ca19349c0b9fab9f4c44e34c
Jakub Kotkowiak <cckotkowiak@gmail.com>
2021-08-31 15:07:18 +0200

    Modify install script, change lintables target

--- .github/actions/installation/install.sh
+++ .github/actions/installation/install.sh
@@ -3,12 +3,12 @@ ANSIBLE_LINT_VERSION=${2:-4.3.7}
 MOLECULE_VERSION=${3:-3.2.1}
 
 sudo apt-get install update -y
-sudo apt-get install pip3
-sudo pip3 install docker==19.03.12-r0
-sudo pip3 install ansible==${ANSIBLE_VERSION}
-sudo pip3 install ansible-lint==${ANSIBLE_LINT_VERSION}
-sudo pip3 install molecule[docker]==${MOLECULE_VERSION}
-sudo pip3 install flake8==3.8.4
+sudo apt-get install pip
+sudo pip install docker==5.0.0
+sudo pip install ansible==${ANSIBLE_VERSION}
+sudo pip install ansible-lint==${ANSIBLE_LINT_VERSION}
+sudo pip install molecule[docker]==${MOLECULE_VERSION}
+sudo pip install flake8==3.8.4
 
 
 ansible --version
