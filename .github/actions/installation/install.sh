595179674e26cd7d0108adef7b5aaa870b42e07b
Jakub Kotkowiak <cckotkowiak@gmail.com>
2021-08-31 14:55:51 +0200

    Start implementing test action

--- .github/actions/installation/install.sh
+++ .github/actions/installation/install.sh
@@ -1,10 +1,15 @@
 ANSIBLE_VERSION=${1:-2.9.6}
 ANSIBLE_LINT_VERSION=${2:-4.3.7}
+MOLECULE_VERSION=${3:-3.2.1}
 
 sudo apt-get install update -y
 sudo apt-get install pip3
+sudo pip3 install docker==19.03.12-r0
 sudo pip3 install ansible==${ANSIBLE_VERSION}
 sudo pip3 install ansible-lint==${ANSIBLE_LINT_VERSION}
+sudo pip3 install molecule[docker]==${MOLECULE_VERSION}
+sudo pip3 install flake8==3.8.4
+
 
 ansible --version
 ansible-lint --version
