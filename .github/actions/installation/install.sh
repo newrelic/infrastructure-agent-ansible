728ab024ead2b0291229c4c239022ed25b3d4945
Jakub Kotkowiak <cckotkowiak@gmail.com>
2021-09-01 14:18:46 +0200

    Remove variables due version differences

--- .github/actions/installation/install.sh
+++ .github/actions/installation/install.sh
@@ -1,11 +1,7 @@
-ANSIBLE_VERSION=${1:-4.5.0}
-ANSIBLE_LINT_VERSION=${2:-5.1.3}
-MOLECULE_VERSION=${3:-3.4.0}
-
 sudo apt-get install update -y
 sudo apt-get install pip
-sudo pip install ansible-lint[community]==5.1.3
-sudo pip install molecule[docker]
+sudo pip install ansible-lint[community]==5.1.3 -v
+sudo pip install molecule[docker] -v
 
 
 ansible --version
