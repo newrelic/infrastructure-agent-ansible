674927c8eb2a655a7380d3385a99ad2d407b733b
Jakub Kotkowiak <cckotkowiak@gmail.com>
2021-09-01 12:22:09 +0200

    Move ansible to one command with ansible-lint

--- .github/actions/installation/install.sh
+++ .github/actions/installation/install.sh
@@ -5,8 +5,7 @@ MOLECULE_VERSION=${3:-3.2.1}
 sudo apt-get install update -y
 sudo apt-get install pip
 sudo pip install docker==5.0.0
-sudo pip install ansible-lint==${ANSIBLE_LINT_VERSION}
-sudo pip install ansible==${ANSIBLE_VERSION}
+sudo pip install ansible-lint==${ANSIBLE_LINT_VERSION} ansible==${ANSIBLE_VERSION}
 sudo pip install molecule[docker]==${MOLECULE_VERSION}
 sudo pip install flake8==3.8.4
 
