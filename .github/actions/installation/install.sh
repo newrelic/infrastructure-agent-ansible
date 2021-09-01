c219f4f303133b1fc88ce3f4189364ede25fd93f
Jakub Kotkowiak <cckotkowiak@gmail.com>
2021-09-01 12:45:56 +0200

    Test different versions

--- .github/actions/installation/install.sh
+++ .github/actions/installation/install.sh
@@ -1,11 +1,11 @@
-ANSIBLE_VERSION=${1:-4.4.0}
-ANSIBLE_LINT_VERSION=${2:-4.3.7}
-MOLECULE_VERSION=${3:-3.2.1}
+ANSIBLE_VERSION=${1:-4.5.0}
+ANSIBLE_LINT_VERSION=${2:-5.1.3}
+MOLECULE_VERSION=${3:-3.4.0}
 
 sudo apt-get install update -y
 sudo apt-get install pip
 sudo pip install docker==5.0.0
-sudo pip install ansible-lint==${ANSIBLE_LINT_VERSION} ansible==${ANSIBLE_VERSION}
+sudo pip install ansible-lint[community]==${ANSIBLE_LINT_VERSION} ansible==${ANSIBLE_VERSION}
 sudo pip install molecule[docker]==${MOLECULE_VERSION}
 sudo pip install flake8==3.8.4
 
