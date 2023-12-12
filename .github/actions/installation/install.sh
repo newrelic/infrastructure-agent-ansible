f6a0f7574d3d75d712557d3d822474aeeae11791
Ruben Ruiz de Gauna <rubenruizdegauna@proton.me>
2023-12-12 10:42:31 +0100

    chore: bump GH action dependencies

--- .github/actions/installation/install.sh
+++ .github/actions/installation/install.sh
@@ -1,7 +1,7 @@
 sudo pipx uninstall ansible-core
 sudo pip3 install 'rich>=10.0.0,<11.0.0'
-sudo pip3 install ansible-lint[community]==5.3.2
-sudo pip3 install molecule[docker]==3.6.0
+sudo pip3 install ansible-lint[community]==6.13.1
+sudo pip3 install molecule[docker]==4.0.4
 
 
 ansible --version
